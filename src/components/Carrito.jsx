1393c268730284f1d37fa542d9059dfde0776727
camilo <cristhian.ver01@gmail.com>
2024-11-23 11:42:50 -0500

    agreglo falla css

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/carrito.css';
+import '../styles/Carrito.css';
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
